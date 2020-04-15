fcc20051dde48e889887a34728b0a30a94988bbd
mirandofrank <mirandofrank@csu.fullerton.edu>
2020-04-14 20:01:43 -0700

    finished hw 5

--- scripts/main.js
+++ scripts/main.js
@@ -4,14 +4,17 @@
     var FORM_SELECTOR = '[data-coffee-order="form"]';
     var PAYMENT_SELECTOR = '[payment-form="form"]'
     var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]'; 
+    var SERVER_URL = 'http://coffeerun-v2-rest-api.herokuapp.com/api/coffeeorders';
     var App = window.App;
     var Truck = App.Truck;
     var DataStore = App.DataStore;
+    var RemoteDataStore = App.RemoteDataStore;
     var FormHandler = App.FormHandler;
     var Validation = App.Validation;
     var CheckList = App.CheckList;
+    var remoteDS = new RemoteDataStore(SERVER_URL);
     var webshim = window.webshim;
-    var myTruck = new Truck('ncc-1701', new DataStore());
+    var myTruck = new Truck('ncc-1701', remoteDS);
     window.myTruck = myTruck;
     var checkList = new CheckList(CHECKLIST_SELECTOR);
     checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
